47a8c5ce9edac32d8ce306b659c76bd806b56d7c
itamar_biron <you@example.com>
2018-12-25 05:00:39 +0200

    basic recursion works !

---  EggsCrushTest/src/Main.java
+++  EggsCrushTest/src/Main.java
@@ -4,39 +4,51 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(height(BigInteger.valueOf(2),BigInteger.valueOf(14)));
-        System.out.println(tryRecursion(7,20));
-        int x=0;
-        for (int i =1 ; i <=19; i ++){
-            x = x + tryRecursion(2,i);
-        }
-        System.out.println(x);
+        System.out.println(height(BigInteger.valueOf(2), BigInteger.valueOf(14)));
+        System.out.println(tryRecursion(4, 20));
+        System.out.println(betterRecursion(6,80));
     }
 
 
     public static BigInteger height(BigInteger n, BigInteger m) {
         BigInteger one = BigInteger.valueOf(1);
-        BigInteger two  = BigInteger.valueOf(2);
+        BigInteger two = BigInteger.valueOf(2);
 
-        BigInteger addition =m.add(one)  ;
+        BigInteger addition = m.add(one);
 
         BigInteger multiplay = m.multiply(addition);
         return multiplay.divide(two); // TODO replace with your solution
     }
 
-    public static int tryRecursion ( int n , int  m){
-        int ans =0;
-        if (m==1 || n ==1){
+    public static int tryRecursion(int n, int m) {
+        int ans = 0;
+        if (m == 1 || n == 1) {
             return m;
         }
-        if ( n == 2 ){
-            return (int)(((m+1)*m)/2.0);
+        if (n == 2) {
+            return (int) (((m + 1) * m) / 2.0);
         }
 
-        for ( int i = 1; i<=m-1; i++){
-
+        for (int i = 1; i <= m - 1; i++) {
+            ans = ans +tryRecursion(n-1,i);
         }
 
-        return tryRecursion(n-1,m)+tryRecursion(n-1,m-1) -m;
+        return ans+m;
+    }
+
+
+
+    public static int betterRecursion(int n, int m) {
+        int ans = 0;
+        if (m == 1 || n == 1) {
+            return m;
+        }
+        if (n == 2) {
+            return (int) (((m + 1) * m) / 2.0);
+        }
+
+
+
+        return betterRecursion(n,m-1)+betterRecursion(n-1,m) +1;
     }
 }
