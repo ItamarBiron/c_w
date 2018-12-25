34bec76c680910e88f39c83b3403f49de64036c7
itamar_biron <you@example.com>
2018-12-25 06:16:45 +0200

    work really fast but need to react +1

---  EggsCrushTest/src/Main.java
+++  EggsCrushTest/src/Main.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class Main {
@@ -5,8 +6,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(height(BigInteger.valueOf(2), BigInteger.valueOf(14)));
-        System.out.println(tryRecursion(4, 20000));
+        System.out.println(tryRecursion(4, 2000));
         System.out.println(betterRecursion(4,17));
+//        System.out.println(calculateFactorial(15000));
+        System.out.println(calculateBinom(2,14));
     }
 
 
@@ -47,8 +50,22 @@ public class Main {
             return (int) (((m + 1) * m) / 2.0);
         }
 
-
-
         return betterRecursion(n,m-1)+betterRecursion(n-1,m-1) +1;
+    }
+
+    private static BigInteger calculateFactorial (int number) {
+        BigInteger factorial = BigInteger.ONE;
+        for (int i = 1; i <= number; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        return factorial;
+    }
+
+    private static BigInteger calculateBinom (int n, int m) {
+        BigInteger factorial = BigInteger.ONE;
+        for (int i = m-n+1; i <= m; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        return factorial.divide(calculateFactorial(n));
     }
 }
