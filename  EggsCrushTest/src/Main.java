49905debbf25bfd840b85511c61b0a956871f751
itamar_biron <you@example.com>
2018-12-31 23:16:31 +0200

    update functions

---  EggsCrushTest/src/Main.java
+++  EggsCrushTest/src/Main.java
@@ -6,10 +6,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(height(BigInteger.valueOf(2), BigInteger.valueOf(14)));
-        System.out.println(tryRecursion(4, 2000));
-        System.out.println(betterRecursion(4,17));
+        System.out.println(tryRecursion(2, 20));
+        System.out.println(betterRecursion(3, 11));
 //        System.out.println(calculateFactorial(15000));
-        System.out.println(calculateBinom(2,14));
+//        System.out.println(calculateBinom(40000,80000));
+        System.out.println(calculateFactorial(80000));
+//        BigInteger[] firstArray = returnFactorialArray(30000);
+//        System.out.println(beterCalculate(400,800));//.mod(BigInteger.valueOf(998244353)));
     }
 
 
@@ -33,12 +36,11 @@ public class Main {
         }
 
         for (int i = 1; i <= m - 1; i++) {
-            ans = ans +tryRecursion(n-1,i);
+            ans = ans + tryRecursion(n - 1, i);
         }
 
-        return ans+m;
+        return ans + m;
     }
-
 
 
     public static int betterRecursion(int n, int m) {
@@ -50,10 +52,10 @@ public class Main {
             return (int) (((m + 1) * m) / 2.0);
         }
 
-        return betterRecursion(n,m-1)+betterRecursion(n-1,m-1) +1;
+        return betterRecursion(n, m - 1) + betterRecursion(n - 1, m - 1) + 1;
     }
 
-    private static BigInteger calculateFactorial (int number) {
+    private static BigInteger calculateFactorial(int number) {
         BigInteger factorial = BigInteger.ONE;
         for (int i = 1; i <= number; i++) {
             factorial = factorial.multiply(BigInteger.valueOf(i));
@@ -61,11 +63,31 @@ public class Main {
         return factorial;
     }
 
-    private static BigInteger calculateBinom (int n, int m) {
+    private static BigInteger calculateBinom(int n, int m) { // is equal to m choose n 
         BigInteger factorial = BigInteger.ONE;
-        for (int i = m-n+1; i <= m; i++) {
+        for (int i = m - n + 1; i <= m; i++) {
             factorial = factorial.multiply(BigInteger.valueOf(i));
         }
         return factorial.divide(calculateFactorial(n));
+    }
+
+    private static BigInteger beterCalculate(int n, int m) {
+        BigInteger one = BigInteger.ONE;
+        BigInteger ans = BigInteger.ONE;
+
+        for (int i = 1; i <= n; i++) {
+            ans = ans.add(calculateBinom(i, m));
+        }
+        return ans.subtract(one);
+    }
+
+    private static BigInteger[] returnFactorialArray(int n) {
+        BigInteger[] array = new BigInteger[n];
+        BigInteger lastCalc = BigInteger.ONE;
+        array[0] = lastCalc;
+        for (int i = 1; i < n; i++) {
+            array[i] = array[i-1].multiply(BigInteger.valueOf(i));
+        }
+        return array;
     }
 }
